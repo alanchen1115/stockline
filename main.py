d6099892d28f5e99337450ea287f1acd69266a32
alanchen1115 <42991041+alanchen1115@users.noreply.github.com>
2026-08-22 05:52:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ system_instruction = (
     "融資融券、內外資籌碼進出及財務資訊，並分析近期公司股市展望，給予投資人具體的專業建議，例如股價支撐或壓力！如果查不到資訊，就回答查無相關資訊"
 )
 
-thinking_config = genai.types.ThinkingConfig(thinking_budget=minimum)
+thinking_config = genai.types.ThinkingConfig(thinking_budget='minimum')
 generation_config = genai.types.GenerateContentConfig(
     max_output_tokens=4000,
     temperature=0.1,
